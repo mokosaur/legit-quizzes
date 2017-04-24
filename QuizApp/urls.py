01d1e61651136853a992370b84739a8f5246fda3
Tomasz Danel <timages94@gmail.com>
2017-04-24 02:12:07 +0200

    Minor visual changes

--- QuizApp/urls.py
+++ QuizApp/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     url(r'^register/$', views.register, name='register'),
     url(r'^quiz/(?P<quiz_title>.+)$', views.quiz, name='quiz'),
     url(r'^results/(?P<quiz_title>.+)$', views.results, name='results'),
-    url(r'^quiz/$', views.show_quizzes, name='show_quizzes'),
+    url(r'^$', views.show_quizzes, name='show_quizzes'),
     url(r'^create-quiz/$', views.create_quiz, name='createquiz')
 ]
